d9b6d30b067f5023bdf31758b81c70f08c666ae7
Tima <tima52rus@gmail.com>
2022-07-10 23:12:22 +0300

    update componennt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,46 @@
-import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, Container, Toolbar, Typography } from '@mui/material';
+import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
   return (
-    <AppBar position="static">
-      <Toolbar>
+    // <Box sx={{ flexGrow: 1 }}>
+    //   <AppBar position="static">
+    //     <Toolbar>
+    //       <Container>
+    //         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+    //           <NavLink to={'/users'}>
+    //             Merehead Test
+    //           </NavLink>
+    //         </Typography>
+    //         <Button color="inherit">
+    //           <NavLink to={'/createUser'}>
+    //             Create User
+    //           </NavLink>
+    //         </Button>
+
+    //       </Container>
+    //     </Toolbar>
+    //   </AppBar>
+    // </Box>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
         <Container>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Merehead Test
-          </Typography>
+          <Toolbar>
+
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <NavLink to={'/'}>
+                Merehead Test
+              </NavLink>
+            </Typography>
+            <Button color="inherit">
+              <NavLink to={'/createUser'}>
+                Create User
+              </NavLink>
+            </Button>
+
+          </Toolbar>
         </Container>
-      </Toolbar>
-    </AppBar>
+      </AppBar>
+    </Box>
   )
 }
