50babdfac50196a8fbcc8d07693aff5bc357307b
Tima <tima52rus@gmail.com>
2022-07-10 23:11:57 +0300

    upd

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { store } from './redux/store'
@@ -8,9 +9,11 @@ import { store } from './redux/store'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
+		<HashRouter>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</HashRouter>
 	</React.StrictMode>
 )
 
