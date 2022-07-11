29d481854c39993fd877a6f86f0b553fe84e204d
Tima <tima52rus@gmail.com>
2022-07-11 12:48:53 +0300

    create two funcs for update user data

--- src/components/User/Users.tsx
+++ src/components/User/Users.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@mui/material"
-import { FC } from "react"
+import { Button, Input } from "@mui/material"
+import { FC, useState } from "react"
 import { useDispatch } from "react-redux"
-import { deleteUser } from "../../redux/actions/appActions"
+import { deleteUser, updateUser } from "../../redux/actions/appActions"
 import './User.css'
 
 type UserCartPropType = {
@@ -13,25 +13,39 @@ type UserCartPropType = {
 
 export const UserCard: FC<UserCartPropType> = ({ name, surname, descr, id }) => {
 
+  const [inputName, setName] = useState<string>(name)
+  const [inputSurname, setSurName] = useState<string>(surname)
+  const [inputDescr, setDescription] = useState<string>(descr)
+  const [editMode, setEditMode] = useState(false)
+
   const dispatch = useDispatch();
+
+  const field = (title: string, changeValue: React.Dispatch<React.SetStateAction<string>>) => {
+    return editMode ? <Input value={title} onChange={(e) => changeValue(e.target.value)} style={{ marginLeft: '5px', height: '35px', marginBottom: '5px' }} size={"small"} /> : <span>{title}</span>
+  }
+
+  const changeEditMode = () => {
+    setEditMode(!editMode)
+    editMode && dispatch(updateUser(id, inputName, inputSurname, inputDescr))
+  }
 
   return (
     <div className="card">
       <div className="card_block">
         <div>
-          <span><b>Name:</b> {name}</span>
+          <span><b>Name:</b>{field(inputName, setName)}</span>
         </div>
         <div>
-          <span><b>Surname:</b> {surname ? surname : 'Not Found'}</span>
+          <span><b>Surname:</b>{field(inputSurname, setSurName)}</span>
         </div>
         <div>
           <span>
-            <b>Description:</b> {descr ? descr : 'Not Found'}
+            <b>Description:</b>{field(inputDescr, setDescription)}
           </span>
         </div>
       </div>
       <div className="card_btn">
-        <Button variant="outlined" style={{ marginRight: '10px' }}>Edit</Button>
+        <Button variant="outlined" style={{ marginRight: '10px' }} onClick={() => changeEditMode()}>Edit</Button>
         <Button variant="contained" onClick={() => dispatch(deleteUser(id))}>Delete</Button>
       </div>
     </div>
